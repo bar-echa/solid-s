5fe48dea694dd251f86fb92520bce365e8f58073
BARECHA ETANA <145106498+bar-echa@users.noreply.github.com>
2024-09-06 21:47:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
- new Swiper('.card-wrapper', {
-    
-    loop: true,
-    spaceBetween: 5,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
-      dynamicBullets:true
-      
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  breakpoints:{
-    0:{
-        slidesPerView: 1
-    },
-    768:{
-        slidesPerView: 2
-    }
-  },
-  1024:{
-    slidesPerView: 3
-}
-}
-  
-  );
-  
+document.addEventListener('DOMContentLoaded', () => {
+    // Swiper initialization code
+    const swiper = new Swiper('.card-wrapper', {
+      loop: true,
+      spaceBetween: 5,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        }
+      }
+    });
+  });
