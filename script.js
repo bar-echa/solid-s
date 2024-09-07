7717ffaf93108b0ee5e675cb76302c978d880e69
BARECHA ETANA <145106498+bar-echa@users.noreply.github.com>
2024-09-07 13:48:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,3 +25,69 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+  
+  var sidemenu = document.getElementById("sidemenu");
+
+  function openmenu() {
+      sidemenu.style.right = "0";
+  }
+  
+  function closemenu() {
+      sidemenu.style.right = "-200px";
+  }
+    const scriptURL = '<SCRIPT URL>'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
+    
+    function myFunction1() {
+      var moreText = document.getElementById("more");
+      var lessText = document.getElementById("less");
+      var btn = document.getElementById("myBtn");
+      
+      if (moreText.style.display === "none") {
+          moreText.style.display = "inline";
+          lessText.style.display = "inline";
+          btn.innerHTML = "Read less";
+      } else {
+          moreText.style.display = "none";
+          lessText.style.display = "none";
+          btn.innerHTML = "Read more";
+      }
+  }
+  
+  function myFunction2() {
+              var moreImages = document.getElementById("more2");
+              var btn = document.getElementById("btn");
+              
+              if (moreImages.style.display === "none" || moreImages.style.display === "") {
+                  moreImages.style.display = "block";
+                  btn.innerHTML = "See Less";
+              } else {
+                  moreImages.style.display = "none";
+                  btn.innerHTML = "See More";
+              }
+          }
+          var tablinks =document.getElementsByClassName("tab-links");
+    var tabcontents =document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    
+   
+    event.stopPropagation();
+}
+
+
